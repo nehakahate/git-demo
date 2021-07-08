1b0e611080da9fa4307650bc49f325e208cad28a
nehakahate <32272413+nehakahate@users.noreply.github.com>
2021-07-08 14:30:43 +0530

    Add files via upload
    Math file added

---
+++ math.py
@@ -0,0 +1,12 @@
+#here again
+def add(x,y): 
+	return x+y
+#Multiply 
+def multiply(x,y): 
+	return x*y  #Bug456 change
+#divide 
+def divide(x,y): 
+	reurn x/y #master 
+#minus
+def subtract(x,y): 
+	return x-y #master change
